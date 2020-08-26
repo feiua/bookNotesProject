da5f3306f9bf6c40f14c19f12b8bfe40a5721c8e
feiua <287221429@qq.com>
2020-08-27 01:39:00 +0800

    pillow 问题应该是pillow更新后有些模块的用法变了，网上查的代码都是老版本的，所以不用pillow算了。本次还增加了Label 的各种用法

--- tkinterPrectices/prac2.py
+++ tkinterPrectices/prac2.py
@@ -10,19 +10,36 @@
 """
 
 from tkinter import *
-from PIL import Image, ImageTk
 import os
+
+
+def callback():
+    var.set('美女好看吗？')
+
 
 root = Tk()
 
-textLabel = Label(root, text='你在看啥呢？')
+frame1 = Frame(root)
+frame2 = Frame(root)
+
+var = StringVar()
+var.set('你在看啥呢？')
+
+textLabel = Label(frame1, textvariable=var,
+                  justify=LEFT, padx=10)
+
 textLabel.pack(side=LEFT)
 
-fileName = os.path.abspath(r'\images\pic1.jpg')
-imageFile = Image.open(fileName)
-tkImage = ImageTk.PhotoImage(imageFile)
+fileName = os.path.abspath(
+    r'D:\我的坚果云\编程\python\practices\myPractice4DataBase\tkinterPrectices\images\pic1.png')
+image = PhotoImage(file=fileName)
+imgLabel = Label(frame1, image=image)
+imgLabel.pack(side=RIGHT)
 
-imgLabel = Label(root, image=tkImage)
-imgLabel.pack()
+button1 = Button(frame2, text='不看美女难道看你？', command=callback)
+button1.pack()
+
+frame1.pack(padx=10, pady=10)
+frame2.pack(padx=10, pady=10)
 
 root.mainloop()
