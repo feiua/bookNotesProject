6e70832de4c05dfb33df70798dd9f4097b90ad76
feiua <287221429@qq.com>
2020-09-10 00:39:58 +0800

    项目更名
    为后续更改了文件名并增加了一些后续开发的包

---
+++ UI/mainInterface.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# _*_ coding.utf-8 _*_
+# @Site: 
+# 开发团队: 待君加入
+# 开发人员：Jianfei
+# 开发时间：2020-09-09 10:54 PM
+# 文件名称：mainInterface.py
+# 开发工具：PyCharm
+
+
+# from tkinter import ttk
+from tkinter import *
+
+
