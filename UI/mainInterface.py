6c0b9b362130a0310a17e40c2f6f9eaa00e41f2e
feiua <287221429@qq.com>
2020-09-14 00:06:33 +0800

    开始写主界面
    思考表格的索引问题

--- UI/mainInterface.py
+++ UI/mainInterface.py
@@ -11,4 +11,20 @@
 # from tkinter import ttk
 from tkinter import *
 
+# 建立主面板
+root = Tk()
 
+# 建立 ribbon
+
+# 建立其他元素
+
+# 建立表格
+headersY = [1, 2, 3]  # 列表头
+headersX = ['a', 'b', 'c']  # 行表头
+tableFrame = Frame(root, width=len(headersY)*45, height=len(headersX)*35)  # 建立表格主面板
+tableFrame.pack()
+
+tableIndices = list()
+for column in headersY:
+    for row in headersX:
+        pass
---
+++ tkinterPrectices/test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# _*_ coding.utf-8 _*_
+# @Site: none
+# 开发团队: 待君加入
+# 开发人员：Jianfei
+# 开发时间：2020-09-13 11:11 PM
+# 文件名称：test.py
+# 开发工具：PyCharm
+
+
+from tkinter import *
+
+root = Tk()
+
+frame = Frame(root, )
