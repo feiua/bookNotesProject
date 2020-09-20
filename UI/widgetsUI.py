c4ab935e252619947b7fc2d6eb48057df7b7a669
feiua <287221429@qq.com>
2020-09-20 19:22:30 +0800

    Create widgetsUI.py

---
+++ UI/widgetsUI.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# _*_ coding.utf-8 _*_
+# @Site    : NA
+# 开发团队: 待君加入
+# 开发人员：Lenovo
+# 开发时间：2020-09-2019:11
+# 文件名称：widgetsUI.py
+# 开发工具：PyCharm
+
+
+from tkinter import *
+
+
+class CellUI:
+    pass
