7d878cd3eaab8f62928808c50ada67765cc37b53
feiua <287221429@qq.com>
2020-10-04 22:50:13 +0800

    完善自制的表格容器
    该容器目前只设计了2维

--- logic/containers.py
+++ logic/containers.py
@@ -16,11 +16,58 @@ import pandas as pd
 import uuid
 
 
-class MyClass:
-    def __init__(self, ID):
-        self.ID = ID
+class MyTable:
+    def __init__(self, cellClass=None, *args, **kwargs):
+        """
+        初始化
+        :param args: 用于实例化 cell 的参数
+        :param kwargs:
+        colNum: 列总数
+        rowNum: 行总数
+        colNames: 列名称
+        rowNames: 行名称
+        """
+        self.cellDict = dict()
+        self.col2CellDict = dict()  # 通过 column 索引 cell
+        self.row2CellDict = dict()  # 通过 row 索引 cell
+        self.cell2ColDict = dict()  # 通过 cell 索引 column
+        self.cell2RowDict = dict()  # 通过 cell 索引 row
+        self.rowNameDict = dict()  # 通过 ID 索引行名称
+        self.colNameDict = dict()  # 通过 ID 索引列名称
+
+        if kwargs.keys() & ('colNum', 'rowNum'):
+            self.rowNum = kwargs['colNum']
+            self.colNum = kwargs['rowNum']
+            for rowName in range(self.rowNum):
+                self.rowNameDict[uuid.uuid1()] = str(rowName)
+            for colName in range(self.colNum):
+                self.colNameDict[uuid.uuid1()] = str(colName)
+        if kwargs.keys() & ('colNames', 'rowNames'):
+            for rowName in kwargs['rowNames']:
+                self.rowNameDict[uuid.uuid1()] = str(rowName)
+            for colName in kwargs['colNames']:
+                self.colNameDict[uuid.uuid1()] = str(colName)
+
+        for rowID in self.rowNameDict.keys():
+            self.row2CellDict[rowID] = []
+            for colID in self.colNameDict.keys():
+                self.col2CellDict[colID] = []
+                cellID = uuid.uuid1()
+                if cellClass:
+                    cell = cellClass(args)
+                    self.cellDict[cellID] = cell
+                self.row2CellDict[rowID].append(cellID)
+                self.col2CellDict[colID].append(cellID)
+                self.cell2RowDict[cellID] = rowID
+                self.cell2ColDict[cellID] = colID
+
+    def __getitem__(self, item):
+        pass
+
+    def __setitem__(self, key, value):
+        pass
 
 
-df = pd.DataFrame(data=[], index=['a', 'c'], columns=['1', '2'])
-
-print(df.loc['a', '1'])
+if __name__ == "__main__":
+    m = MyTable(colNum=1, rowNum=2)
+    print(m.rowNum, m.colNum)
