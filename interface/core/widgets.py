f0e508e4dc1367d530d2e503d9f20a134db3319d
feiua <287221429@qq.com>
2021-10-20 22:59:06 +0800

    unnamed
    查到 Toplevel.destroy 方法可以关闭窗口，https://www.orcode.com/question/1243110_k6a450.html

--- interface/core/widgets.py
+++ interface/core/widgets.py
@@ -18,15 +18,41 @@ def callback():
     print('Hi')
 
 
+def createNewFile():
+    top = Toplevel()
+    top.title('Python')
+
+    Label(top, text='the work: ').grid(row=0, column=0)
+    Label(top, text='the author: ').grid(row=1, column=0)
+
+    e1 = Entry(top)
+    e2 = Entry(top)
+    e1.grid(row=0, column=1, padx=10, pady=5)
+    e2.grid(row=1, column=1, padx=10, pady=5)
+
+    def show():
+        print('the work: %s ' % e1.get())
+        print('the author: %s ' % e2.get())
+
+    Button(top, text='get information', width=10, command=show) \
+        .grid(row=2, column=0, sticky=W, padx=10, pady=5)
+    Button(top, text='exit', width=10, command=top.) \
+        .grid(row=2, column=1, sticky=E, padx=10, pady=5)
+
+
 menuBar = Menu(root)
 
 # command
 fileMenu = Menu(menuBar, tearoff=FALSE)
+fileMenu.add_command(label='新建', command=createNewFile)
 fileMenu.add_command(label='打开', command=callback)
 fileMenu.add_command(label='保存', command=callback)
 fileMenu.add_separator()
 fileMenu.add_command(label='退出', command=root.quit)
 menuBar.add_cascade(label='文件', menu=fileMenu)
+
+
+
 
 
 root.config(menu=menuBar)
