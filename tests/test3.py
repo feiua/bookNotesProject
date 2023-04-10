0fd5b27339fa89b3afe16e099f2a839376dd5859
feiua <287221429@qq.com>
2023-04-11 00:27:48 +0800

    继续研究可视化

--- tests/test3.py
+++ tests/test3.py
@@ -7,57 +7,21 @@
 # 3. Try selecting some code and hitting edit. Ask the bot to add residual layers.
 # 4. To try out cursor on your own projects, go to the file menu (top left) and open a folder.
 
-import tkinter as tk
-from tkinter import filedialog
+import sqlite3
 
-class Notepad:
-    def __init__(self, master):
-        self.master = master
-        master.title("Notepad")
+# the default database opened is "mydatabase.db"
+conn = sqlite3.connect(r'D:/UserFiles/文档\GitHub/bookNotesProject/db/data/mydatabase.db')
+c = conn.cursor()
+c.execute('pragma table_info(event_table)')
+data = c.fetchall()
+col_names = [x[1] for x in data]
+print(col_names)
 
-        self.textarea = tk.Text(master)
-        self.textarea.pack(fill=tk.BOTH, expand=True)
-
-        self.menu = tk.Menu(master)
-        self.file_menu = tk.Menu(self.menu, tearoff=0)
-        self.file_menu.add_command(label="New", command=self.new_file)
-        self.file_menu.add_command(label="Open", command=self.open_file)
-        self.menu.add_cascade(label="File", menu=self.file_menu)
-
-        master.config(menu=self.menu)
-
-    def open_file(self):
-        file_path = filedialog.askopenfilename()
-        if file_path:
-            with open(file_path, "r") as file:
-                self.textarea.delete(1.0, tk.END)
-                self.textarea.insert(tk.END, file.read())
-
-    def new_file(self):
-        self.textarea.delete(1.0, tk.END)
-        self.master.title("Untitled - Notepad")
-        self.file_path = None
-        self.dialog = tk.Toplevel()
-        self.dialog.title("New File")
-        tk.Label(self.dialog, text="Title:").grid(row=0, column=0, sticky="w")
-        tk.Label(self.dialog, text="Time:").grid(row=1, column=0, sticky="w")
-        tk.Label(self.dialog, text="Note:").grid(row=2, column=0, sticky="w")
-        self.title_entry = tk.Entry(self.dialog)
-        self.time_entry = tk.Entry(self.dialog)
-        self.note_entry = tk.Entry(self.dialog)
-        self.title_entry.grid(row=0, column=1, padx=5, pady=5)
-        self.time_entry.grid(row=1, column=1, padx=5, pady=5)
-        self.note_entry.grid(row=2, column=1, padx=5, pady=5)
-        tk.Button(self.dialog, text="保存", command=self.save_file).grid(row=3, column=0, sticky="w")
-        tk.Button(self.dialog, text="取消", command=self.dialog.destroy).grid(row=3, column=1, sticky="e")
-
-    def save_file(self):
-            self.file_path = filedialog.asksaveasfilename(defaultextension=".txt")
-            if self.file_path:
-                with open(self.file_path, "w") as file:
-                    file.write(self.textarea.get(1.0, tk.END))
+# print rows of event_table
+c.execute('SELECT * FROM event_table')
+rows = c.fetchall()
+for row in rows:
+    print(row)
 
 
-root = tk.Tk()
-notepad = Notepad(root)
-root.mainloop()
+conn.close()
