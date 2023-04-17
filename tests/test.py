adf3b1b4c54e7687bfdf2fcad46fe5adfb007af5
feiua <287221429@qq.com>
2023-04-17 08:15:15 +0800

    尝试 PyQt置换原代码
    1. 第一次尝试

--- tests/test.py
+++ tests/test.py
@@ -1,94 +1,43 @@
-#!/usr/bin/env python
-# _*_ coding.utf-8 _*_
-# @Site    : 
-# 开发团队: 待君加入
-# 开发人员：Lenovo
-# 开发时间：2020-09-2616:18
-# 文件名称：test.py
-# 开发工具：PyCharm
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QAction, QFileDialog
 
 
-import sqlite3
-import os
-import io
-import tkinter as tk
-from tkinter import ttk
-from PIL import Image, ImageTk
+class Notebook(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('File')
+
+        newAction = QAction('New', self)
+        newAction.triggered.connect(self.newFile)
+        fileMenu.addAction(newAction)
+
+        openAction = QAction('Open', self)
+        openAction.triggered.connect(self.openFile)
+        fileMenu.addAction(openAction)
+
+        self.setGeometry(300, 300, 350, 300)
+        self.setWindowTitle('Notebook')
+        self.show()
+
+    def newFile(self):
+        # Code to create a new file goes here
+        pass
+
+    def openFile(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        fileName, _ = QFileDialog.getOpenFileName(self, "Open File", "", "All Files (*);;Text Files (*.txt)",
+                                                  options=options)
+        if fileName:
+            # Code to open the selected file goes here
+            pass
 
 
-def zoom_image(img, new_width):
-    """
-    Keep the aspect ratio of image
-    :param img: img = Image.open('path/to/image.jpg')
-    :param new_width: value of new width set
-    :return: the image with new size
-    """
-
-    # Get the original size of the image
-    width, height = img.size
-
-    # Calculate the aspect ratio of the image
-    aspect_ratio = width / height
-
-    # Calculate the new height of the image based on a width of 100 pixels
-    new_height = int(new_width / aspect_ratio)
-
-    # Resize the image using Lanczos resampling
-    img = img.resize((100, new_height), Image.LANCZOS)
-
-    # Create a Tkinter PhotoImage object from the resized image
-    return img
-
-# Connect to the database
-database_path = r'D:/UserFiles/文档/GitHub/bookNotesProject/db/data/mydatabase.db'
-conn = sqlite3.connect(database_path)
-c = conn.cursor()
-
-# Retrieve the image file paths from the database
-c.execute("SELECT data FROM image_table WHERE event_id=?", ('940ade2b-f141-4152-b250-11b972776184',))
-images_data = [row[0] for row in c.fetchall()]
-
-root = tk.Tk()
-mother_frame = tk.Frame(root)
-mother_frame.pack()
-button = ttk.Button(mother_frame, text="Hover over me")
-button.pack()
-
-preview_window = tk.Toplevel()
-preview_window.destroy()
-
-def show_preview(event):
-    # Create a new window to display the preview
-    preview_window = tk.Toplevel()
-    preview_window.title("Image Preview")
-
-    # Create a canvas to display the images
-    canvas = tk.Canvas(preview_window, bg='yellow', width=110 * len(images_data), height=300)
-    canvas.pack()
-
-    # 在画布中画出一个矩形，fill为矩形填充的颜色，outline边界颜色且width宽度
-    canvas.create_rectangle(50, 50, 150, 150, fill='blue', outline='green', width=2)
-
-    x_coordinate = 0
-    photo_list = []
-    new_width = 100
-    for i, image_data in enumerate(images_data):
-        # Create the Pillow image and Tkinter PhotoImage objects
-        img = Image.open(io.BytesIO(image_data))
-        # print(x_coordinate, x_coordinate+new_width)
-        img = zoom_image(img, new_width)
-        photo_list.append(ImageTk.PhotoImage(img))
-
-        # Display the image on the canvas
-        canvas.create_image(x_coordinate, 0, image=photo_list[i], anchor=tk.NW)
-        x_coordinate += 10 + new_width
-
-
-def hide_preview(event):
-    # Code to hide preview of images
-    preview_window.destroy()
-
-button.bind("<Enter>", show_preview)
-button.bind("<Leave>", hide_preview)
-
-root.mainloop()
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    notebook = Notebook()
+    sys.exit(app.exec_())
